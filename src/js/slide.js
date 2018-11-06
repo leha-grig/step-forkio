a6430017435aed3b10d94117dc8f6fa5180bd10b
Oleksii Grygorov <leha.grig@gmail.com>
2018-11-06 21:00:34 +0200

    main - all src, gulp-building

---
+++ src/js/slide.js
@@ -0,0 +1,29 @@
+ function Slider(){
+    this.slideIndex = 1;
+     let self = this;
+    /*this.showSlides(slideIndex);*/
+
+    this.plusSlides = function (n) {
+        this.showSlides(self.slideIndex += n);
+    }
+
+    this.showSlides = function (n) {
+
+        let slides = document.getElementsByClassName("slidershow-container__mySlides");
+
+
+        if (n > slides.length) {
+            self.slideIndex = 1;
+        }
+        if (n < 1) {
+            self.slideIndex = slides.length;
+        }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slides[self.slideIndex - 1].style.display = "block";
+    }
+}
+let slider = new Slider();
+slider.showSlides();
